ad3bfe978f191ee2f1b4171e23fcb957da64eff2
Antoine Bernard <antoine.bernard@ensilog.com>
2023-06-08 00:47:46 +0200

    Implemented AddContactToCompanyByEmail action

---
+++ Ensilog.Engagebay.Tests/Companies/AddContactToCompanyByEmailTests.cs
@@ -0,0 +1,59 @@
+﻿using Ensilog.Engagebay.Companies.Exceptions;
+using Ensilog.Engagebay.Companies;
+using FluentAssertions;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ensilog.Engagebay.Contacts.Exceptions;
+
+namespace Ensilog.Engagebay.Tests.Companies
+{
+    public class AddContactToCompanyByEmailTests
+    {
+        [Fact]
+        public void Constructor_ValidInput_ConstructsSuccessfully()
+        {
+            // Arrange
+            long companyId = 567436789433;
+            string contactEmail = "samson@walt.ltd";
+
+            // Act
+            var command = new AddContactToCompanyByEmail(companyId, contactEmail);
+
+            // Assert
+            command.Should().NotBeNull();
+            command.Uri.Should().Be($"/dev/api/panel/companies/{companyId}/add-contact-by-contactEmail");
+            command.Method.Should().Be(Method.Post);
+            command.ContentType.Should().Be("application/x-www-form-urlencoded");
+            command.Body.Should().Be($"companyId={companyId}&contactEmail={contactEmail}");
+        }
+
+        [Fact]
+        public void Constructor_NegativeCompanyId_ThrowsException()
+        {
+            // Arrange
+            long companyId = -1;
+            string contactEmail = "samson@walt.ltd";
+
+            // Act & Assert
+            Action act = () => new AddContactToCompanyByEmail(companyId, contactEmail);
+            act.Should().Throw<CompanyIdInvalidException>();
+        }
+
+        [Fact]
+        public void Constructor_InvalidEmail_ThrowsException()
+        {
+            // Arrange
+            long companyId = 567436789433;
+            string contactEmail = "";
+
+            // Act & Assert
+            Action act = () => new AddContactToCompanyByEmail(companyId, contactEmail);
+            act.Should().Throw<ContactEmailNullException>();
+        }
+    }
+
+}
---
+++ Ensilog.Engagebay/Companies/AddContactToCompanyByEmail.cs
@@ -0,0 +1,33 @@
+﻿using Ensilog.Engagebay.Abstractions;
+using Ensilog.Engagebay.Companies.Exceptions;
+using Ensilog.Engagebay.Contacts.Exceptions;
+using RestSharp;
+using System.Text.RegularExpressions;
+
+namespace Ensilog.Engagebay.Companies
+{
+    public class AddContactToCompanyByEmail : EngageBayCommand
+    {
+        public override string Uri => $"/dev/api/panel/companies/{_companyId}/add-contact-by-contactEmail";
+        public override Method Method => Method.Post;
+        public override string ContentType => "application/x-www-form-urlencoded";
+
+        public override string Body => $"companyId={_companyId}&contactEmail={_contactEmail}";
+
+        private long _companyId;
+        private string _contactEmail;
+
+        public AddContactToCompanyByEmail(long companyId, string contactEmail)
+        {
+            if (companyId < 0)
+                throw new CompanyIdInvalidException();
+
+            if (string.IsNullOrEmpty(contactEmail))
+                throw new ContactEmailNullException();
+
+            _companyId = companyId;
+            _contactEmail = contactEmail;
+        }
+    }
+
+}
